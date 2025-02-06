7f23c9abf572268a8a25b77fd47ed87302568e26
Bashari Rivka <basrivka1@gmail.com>
2025-02-07 00:12:38 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './cmps/AppHeader.vue'
+import AppFooter from './cmps/AppFooter.vue'
+
+export default{
+  components: {
+    AppHeader,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <RouterView />
+  </main>
+  <AppFooter />
+</template>
+
+<style scoped></style>
---
+++ src/cmps/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>Mr Bitcoin</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: rgb(114, 190, 118);
+}
+
+a{
+  margin-inline-start: 10px;
+  font-size: 20px;
+
+}
+
+a:hover{
+	cursor: pointer;
+  color: rgb(142, 220, 179);
+}
+.router-link-exact-active{
+  color: rgb(202, 237, 204);
+
+}
+</style>
