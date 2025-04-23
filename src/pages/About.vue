699ce608c54df7589414c3f4db900245526e31d0
Bashari Rivka <basrivka1@gmail.com>
2025-04-23 13:35:01 +0300

    Add content to About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>About Mr. BitCoin</h2>
+    <p>
+Mr. BitCoin is a digital wallet-style application that lets users manage a list of contacts and simulate Bitcoin transfers in a simple, user-friendly interface.
+You can add, edit, and delete contacts, view your balance, and keep track of recent transactions.
+The app also displays real-time Bitcoin exchange rates and market volume to help users stay updated on the latest trends.
+</p>
+<h4> Enjoy exploring the world of Bitcoin with a smooth and intuitive experience!
+</h4>
   </div>
 </template>
 
@@ -12,4 +19,12 @@
     align-items: center;
   }
 }
+
+.about{
+  padding: 40px;
+}
+
+ h2{
+    margin-bottom:15px;
+  }
 </style>
