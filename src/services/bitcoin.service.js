9b0e26475468b9a72fed39810f585be02cbe3832
Bashari Rivka <basrivka1@gmail.com>
2025-04-24 11:23:23 +0300

    Fix functions

--- src/services/bitcoin.service.js
+++ src/services/bitcoin.service.js
@@ -6,19 +6,7 @@ const TRADE_VOLUME_KEY = 'tradeVolume'
 export const bitcoinService = {
     getRate,
     getTradeVolume,
-    getMarketPriceHistory,
-    getAvgBlockSize,
-
 }
-
-// export const tradeVolume {
-//     values: { x: number, y: number }[]
-// }
-
-// export const tradeResult {
-//     name: string
-//     value: number
-// }
 
 function getRate(coins) {
     return axios.get(`https://blockchain.info/tobtc?currency=USD&value=${coins}`)
@@ -51,24 +39,6 @@ function getTradeVolume() {
       throw error;
     });
 }
-  
-  
-
-// function getTradeVolume(name, value){
-//     const data = storageService.load(TRADE_VOLUME_KEY)
-//     if (data) return Promise.resolve(data)
-//     return axios.get(`https://api.blockchain.info/charts/trade-volume?timespan=5months&format=json&cors=true`)
-//             .then(res => {
-//                 const vals = _formatTradeVolumeForChart(res)
-//                 storageService.store(TRADE_VOLUME_KEY, vals)
-//                 return vals
-//                 // return res.data;
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching data:', error);
-//                 throw error;
-//             });   
-// }
 
 // Private functions
 function _formatTradeVolumeForChart(res) {
@@ -84,12 +54,3 @@ function _formatTradeVolumeForChart(res) {
 }
 
 
-function getAvgBlockSize() {
-
-    return this.http.get(`https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true`)
-}
-
-function getMarketPriceHistory() {
-
-    return this.http.get(`https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true`)
-}
